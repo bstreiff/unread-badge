1f16733ae8e54ece0bf9b06f3e4f778a00585935
Brandon Streiff <git@streiff.net>
2014-04-27 22:13:26 -0500

    Remove Timer.jsm reference.
    I had added this in a prior change for code I ultimately did not use.
    Remove the import(), since it is no longer necessary.

--- bootstrap.js
+++ bootstrap.js
@@ -67,7 +67,6 @@ net.streiff.unreadbadge = function()
 
    Components.utils.import("resource://gre/modules/Services.jsm");
    Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
-   Components.utils.import("resource://gre/modules/Timer.jsm");
 
    var xpc = {};
    
