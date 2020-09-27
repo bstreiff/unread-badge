452c6747cc6a21f6c1e8b407fb87c18f71e03a33
Brandon Streiff <git@streiff.net>
2020-09-27 13:47:39 -0500

    Update build script to give me .xpi output

--- build.bat
+++ build.bat
@@ -5,3 +5,7 @@ del "xpi/unread-badge@streiff.net.zip"
 cd src
 "C:\Program Files\7-Zip\7z.exe" a "../xpi/unread-badge@streiff.net.zip" .
 cd ..
+cd xpi
+del "unread-badge@streiff.net.xpi"
+rename "unread-badge@streiff.net.zip" "unread-badge@streiff.net.xpi"
+cd ..
