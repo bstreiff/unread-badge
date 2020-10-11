6b05404ff424324dc9d683e531198ce7776b87dc
Brandon Streiff <git@streiff.net>
2020-10-11 13:12:26 -0500

    Add a build script for `npm run build`
    This replaces the old batch file, which relied on 7zip being installed.

---
+++ build.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const archiver = require("archiver");
+
+const prefix = "windows_taskbar_unread_badge"
+const suffix = "tb-windows"
+const manifest = JSON.parse(fs.readFileSync("src/manifest.json"));
+const version = manifest["version"]
+const outname = "/xpi/" + prefix + "-" + version + "-" + suffix + ".xpi";
+
+fs.mkdirSync(__dirname + "/xpi", { recursive: true });
+
+const output = fs.createWriteStream(__dirname + outname);
+const archive = archiver("zip", {});
+
+archive.on("end", function() {
+  console.log(archive.pointer() + " total bytes written to " + outname);
+});
+
+archive.on("error", function(err) {
+  throw err;
+});
+
+archive.pipe(output);
+archive.directory("src/", false);
+archive.finalize();
+
